b149f55e56eb36e938a22898a39671186e34f574
Jakob Frick <jakob.maria.frick@gmail.com>
2024-11-24 21:20:09 -0800

    Initial Commit

---
+++ countdown/countdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  countdownApp.swift
+//  countdown
+//
+//  Created by Jakob Frick on 24/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct countdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ countdownTests/countdownTests.swift
@@ -0,0 +1,17 @@
+//
+//  countdownTests.swift
+//  countdownTests
+//
+//  Created by Jakob Frick on 24/11/2024.
+//
+
+import Testing
+@testable import countdown
+
+struct countdownTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
