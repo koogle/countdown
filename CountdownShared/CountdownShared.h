2ddf0fe00c381d421e502e5951bbb2d75fc60553
Jakob Frick <jakob.maria.frick@gmail.com>
2024-11-25 13:16:40 -0800

    update

---
+++ CountdownShared/CountdownShared.h
@@ -0,0 +1,16 @@
+//
+//  CountdownShared.h
+//  CountdownShared
+//
+//  Created by Jakob Frick on 24/11/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CountdownShared.
+FOUNDATION_EXPORT double CountdownSharedVersionNumber;
+
+//! Project version string for CountdownShared.
+FOUNDATION_EXPORT const unsigned char CountdownSharedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CountdownShared/PublicHeader.h>
